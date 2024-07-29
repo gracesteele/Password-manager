eae8f1c405b60c4781458893bbafefcec5de76d6
Grace Steele <gssteeleuk@gmail.com>
2024-07-29 13:09:52 +0100

    Further reduced password.py file

--- password.py
+++ password.py
@@ -1,4 +1,4 @@
-import random
+from random import choice, randint, shuffle
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -6,20 +6,16 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
 
 def generate_password():
 
-    password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-    password_nums = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_nums = [choice(numbers) for _ in range(randint(2, 4))]
 
     password_list = password_letters + password_symbols + password_nums
 
-    random.shuffle(password_list)
+    shuffle(password_list)
 
     password = ""
     for char in password_list:
