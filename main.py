55ff8c6d96d07b6c57198a39d98cdbb8524b53c4
Grace Steele <gssteeleuk@gmail.com>
2024-07-29 10:28:53 +0100

    Labels, entries, and buttons added

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Create canvas
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid()
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+website_input = Entry(width=50)
+website_input.grid(row=1, column=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+email_input = Entry(width=50)
+email_input.grid(row=2, column=1, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+password_input = Entry(width=32)
+password_input.grid(row=3, column=1)
+gen_password = Button(text="Generate Password")
+gen_password.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=42)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
