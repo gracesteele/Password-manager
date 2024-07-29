3398ddf78189c21be929e9bb2cbdc7f3677d33e6
Grace Steele <gssteeleuk@gmail.com>
2024-07-29 11:51:06 +0100

    Reformatted UI Setup and added focus to website entry

--- main.py
+++ main.py
@@ -15,20 +15,28 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-website_input = Entry(width=50)
-website_input.grid(row=1, column=1, columnspan=2)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-email_input = Entry(width=50)
-email_input.grid(row=2, column=1, columnspan=2)
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
+
+# Entries
+website_input = Entry(width=50)
+website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
+
+email_input = Entry(width=50)
+email_input.grid(row=2, column=1, columnspan=2)
+
 password_input = Entry(width=32)
 password_input.grid(row=3, column=1)
+
+# Buttons
 gen_password = Button(text="Generate Password")
 gen_password.grid(row=3, column=2)
 
